c2414c9ec62744cb7225cce381742302045907b0
Temateu Roslyn <temateuroslynf32@gmail.com>
2023-02-25 03:18:30 +0100

    adding java-run.sh file

---
+++ java-run.sh
@@ -0,0 +1,5 @@
+cd src/backend/java
+
+mvn clean package
+
+java -jar target/big-data-training-java-kafka-1.0-SNAPSHOT.jar
--- src/backend/java/src/main/java/com/softmaes/BigDataTrainingJavaApplication.java
+++ src/backend/java/src/main/java/com/softmaes/BigDataTrainingJavaApplication.java
@@ -4,10 +4,10 @@ package com.softmaes;
  * Hello world!
  *
  */
-public class App 
+public class BigDataTrainingJavaApplication 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!!" );
     }
 }
